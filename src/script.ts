c57e500e08056a87962ad0df7e03027bacba9f61
Lucas Almeida <lelukass@gmail.com>
2025-10-14 11:35:36 -0300

    Rename and move file

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,8 @@
 import { Builder, By, until } from 'selenium-webdriver'
 import chrome from 'selenium-webdriver/chrome'
 import path from 'path'
-import dados from './dados.json'
-import { checkData, checkEnv } from './src/utils'
+import dados from '../dados.json'
+import { checkData, checkEnv } from './utils'
 import { config } from 'dotenv'
 ;(async () => {
   config()
